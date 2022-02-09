a765b08fbe4c5a31227b978222a9f55bca967ec6
popularkoju <webpk144@gmail.com>
2022-02-09 16:12:10 +0545

    colors index

--- lib/backend/database.dart
+++ lib/backend/database.dart
@@ -3,11 +3,24 @@ import 'package:flutter/material.dart';
 String localhost = "http://192.168.0.115:8000";
 
 List<Color> colorIndex = [
-  Colors.red,
   Colors.green,
   Colors.yellow,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.pink,
+  Colors.blue,
+  Colors.purple,
   Colors.blueGrey,
   Colors.brown,
+  Colors.teal,
+  Colors.lightGreen,
+  Colors.deepPurple,
+  Colors.indigo,
+  Colors.indigoAccent,
+  Colors.lightGreenAccent,
+  Colors.tealAccent,
+  Colors.deepPurpleAccent,
+
 ];
 
 
