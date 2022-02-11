e1ca40c6dbab9d2354b6d6790441490493434ebb
popularkoju <webpk144@gmail.com>
2022-02-11 14:38:37 +0545

    keyboard binding

--- lib/addBeatNameDialog.dart
+++ lib/addBeatNameDialog.dart
@@ -6,131 +6,123 @@ import 'backend/Entities/Outlet.dart';
 import 'backend/shortestPath.dart';
 
 class AddBeatDialogBox extends StatefulWidget {
-  TextEditingController textController;
-  List<Outlet> rangeIndexes;
-  List<Beat> blueIndexes;
-  List<Outlet> redPositions;
+ final  TextEditingController textController;
+ final  List<Outlet> rangeIndexes;
+  final List<Beat> blueIndexes;
+  final List<Outlet> redPositions;
   final Function setTempRedRadius;
-  AddBeatDialogBox(this.textController, this.rangeIndexes,this.blueIndexes,this.redPositions, this.setTempRedRadius);
-  @override
+
+  AddBeatDialogBox(this.textController, this.rangeIndexes, this.blueIndexes,
+      this.redPositions, this.setTempRedRadius);
+
+
   @override
   State<AddBeatDialogBox> createState() => _AddBeatDialogBoxState();
 }
-toBeatList( rangeIndexes, blueIndexes, textController, setTempRedRadius,redPositions, context, validate) {
-  if (textController
-      .text ==
-      "") {
-    validate = true;
-  } else {
-    validate = false;
-  }
 
 
-  if (validate ==
-      false) {
+
+
+
+class AddtoBeatIntent extends Intent {}
+
+class _AddBeatDialogBoxState extends State<AddBeatDialogBox> {
+  bool validate = false;
+  void toBeatList(
+      rangeIndexes,
+      blueIndexes,
+      textController,
+      setTempRedRadius,
+      redPositions,
+      context,
+      ) {
     rangeIndexes = [];
 
     blueIndexes.add(
-      Beat(
-          textController
-              .text,
-          shortestPath(
-              redPositions)),
+      Beat(textController.text, shortestPath(redPositions)),
     );
-    setTempRedRadius(
-        0.0);
-    Navigator.pop(
-        context);
+    setTempRedRadius(0.0);
+    Navigator.pop(context);
   }
-}
-
-class AddtoBeatIntent  extends Intent{}
-
-
-class _AddBeatDialogBoxState extends State<AddBeatDialogBox> {
-  bool _validate = false;
   @override
   Widget build(BuildContext context) {
     return Shortcuts(
-      shortcuts:{
-        LogicalKeySet(LogicalKeyboardKey.enter):
-            AddtoBeatIntent()
-      },
+      shortcuts: {LogicalKeySet(LogicalKeyboardKey.enter): AddtoBeatIntent()},
       child: Actions(
-        actions:{ AddtoBeatIntent: CallbackAction(
-          onInvoke: (intent) {
-            print ("Added");
+        actions: {
+          AddtoBeatIntent: CallbackAction(onInvoke: (intent) {
+            print("Added");
 
-      }
-        ),
-    },
+            setState(() {
+              if (widget.textController.text == "") {
+                validate = true;
+                print(validate.toString() + " on level 1");
+              } else {
+                validate = false;
+              }
+            });
 
+            if (validate == false) {
+              toBeatList(
+                  widget.rangeIndexes,
+                  widget.blueIndexes,
+                  widget.textController,
+                  widget.setTempRedRadius,
+                  widget.redPositions,
+                  context);
+            }
+          }),
+        },
         child: Center(
           child: Material(
             color: Colors.white,
             child: Padding(
-              padding:
-              const EdgeInsets.all(12.0),
+              padding: const EdgeInsets.all(12.0),
               child: SizedBox(
                 height: 150,
                 width: 300,
                 child: Row(
                   children: [
                     Expanded(
-                      child: TextField(
-                        controller:
-                        widget.textController,
-                        decoration:
-                        InputDecoration(
-                          errorText: _validate ==
-                              true
-                              ? 'Field Can\'t Be Empty'
-                              : null,
+                      child: Focus(
+                        autofocus: true,
+                        child: TextField(
 
-
-                          label:
-                              Text("beat name"),
+                          controller: widget.textController,
+                          decoration: InputDecoration(
+                            errorText:
+                                validate == true ? 'Field Can\'t Be Empty' : null,
+                            label: Text("beat name"),
+                          ),
                         ),
                       ),
                     ),
-                    IconButton(
+                    Focus(
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (widget.textController.text == "") {
+                              validate = true;
+                              print(validate.toString() + " on level 1");
+                            } else {
+                              validate = false;
+                            }
+                          });
 
-                      onPressed: () {
-                        // setState(() {
-                        //   if (widget.textController
-                        //       .text ==
-                        //       "") {
-                        //     _validate = true;
-                        //     print(_validate.toString() + " on level 1");
-                        //   } else {
-                        //     _validate = false;
-                        //   }
-                        // });
-                        //
-                        // if (_validate ==
-                        //     false) {
-                          toBeatList(widget.rangeIndexes, widget.blueIndexes, widget.textController, widget.setTempRedRadius, widget.redPositions, context, _validate);
-                        // widget.rangeIndexes = [];
-                        //
-                        //   widget.blueIndexes.add(
-                        //     Beat(
-                        //         widget.textController
-                        //             .text,
-                        //         shortestPath(
-                        //             widget.redPositions)),
-                        //   );
-                        //   widget
-                        //       .setTempRedRadius(
-                        //       0.0);
-                        //   Navigator.pop(
-                        //       context);
-                        // }
-
-
-                      },
-                      color: Colors.blue,
-                      icon: Icon(
-                        Icons.send,
+                          if (validate == false) {
+                            toBeatList(
+                                widget.rangeIndexes,
+                                widget.blueIndexes,
+                                widget.textController,
+                                widget.setTempRedRadius,
+                                widget.redPositions,
+                                context);
+                          }
+                        },
+                        color: Colors.blue,
+                        icon: Icon(
+                          Icons.send,
+                        ),
                       ),
                     ),
                   ],
